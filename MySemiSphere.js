6da2e5704f22f1f2ca824ae8d7c8960da73c5f68
Rúben Almeida <48838522+arubenruben@users.noreply.github.com>
2019-10-01 16:17:36 +0100

    Ja so falta nao repetir ultimos vertices do lado negativo

--- MySemiSphere.js
+++ MySemiSphere.js
@@ -43,9 +43,6 @@ class MySemiSphere extends CGFobject {
 
                 this.vertices.push(x, y, z);
 
-
-
-
                 /*Em cima so desenha um triangulo*/
 
                 //Ate la desenha retangulos
@@ -67,60 +64,79 @@ class MySemiSphere extends CGFobject {
 
             }
 
-            
+
             if (i < this.stacks - 1) {
                 j--;
                 this.indices.push(i * this.stacks + j, i * this.stacks, (i + 1) * this.stacks + j);
                 this.indices.push((i + 1) * this.stacks + j, i * this.stacks, (i + 1) * this.stacks);
             }
-            
-            
-            
+
         }
-        
-        this.vertices.push(0, 0, 1);
+
+        this.vertices.push(0, 0, this.r);
+        this.normals.push(0, 0, 1);
         i--;
-        for(var k=0;k<this.slices-1;k++){
+        for (var k = 0; k < this.slices - 1; k++) {
 
-            this.indices.push(i*this.stacks+k-1,i*this.stacks+k,i*this.stacks+this.slices-1);
+            this.indices.push(i * this.stacks + k - 1, i * this.stacks + k, i * this.stacks + this.slices - 1);
 
         }
 
 
+        let count_n_vertices = this.vertices.length / 3;
 
-        // for (var i = 0; i <= this.stacks + 1; i++) {
+        for (var i = 0; i < this.stacks; i++) {
 
-        //     teta = i * increment_teta;
+            teta = i * increment_teta;
 
-        //     for (var j = 0; j <= this.slices; j++) {
+            for (var j = 0; j < this.slices; j++) {
 
-        //         fi = j * increment_fi;
+                fi = j * increment_fi;
 
-        //         x = this.r * Math.cos(teta) * Math.cos(fi);
-        //         y = this.r * Math.cos(teta) * Math.sin(fi);
-        //         z = -this.r * Math.sin(teta);
+                x = this.r * Math.cos(teta) * Math.cos(fi);
+                y = this.r * Math.cos(teta) * Math.sin(fi);
+                z = -this.r * Math.sin(teta);
 
-        //         this.vertices.push(x, y, z);
+                this.vertices.push(x, y, z);
 
-        //         //Na primeira iteracao nao iremos desenhar   
+                /*Em cima so desenha um triangulo*/
 
-        //         if (i <= this.stacks) {
-        //             this.indices.push(i * this.stacks + j, (i + 1) * this.stacks + j + 1, i * this.stacks + j + 1);
-        //             this.indices.push((i + 1) * this.stacks + j, (i + 1) * this.stacks + j + 1, i * this.stacks + j);
-        //         }
+                //Ate la desenha retangulos
 
-        //         this.normals.push(x / this.r, y / this.r, z / this.r);
-        //         this.texCoords1 = [
-        //             0, 0,
-        //             0, 1,
-        //             1, 0,
-        //             1, 1
-        //         ];
-        //         this.texCoords.push(...this.texCoords1);
+                if (i < this.stacks - 1 && j < this.slices - 1) {
+                    this.indices.push(i * this.stacks + j + count_n_vertices, (i + 1) * this.stacks + j + count_n_vertices, i * this.stacks + j + 1 + count_n_vertices);
 
-        //     }
+                    this.indices.push((i + 1) * this.stacks + j + count_n_vertices, (i + 1) * this.stacks + j + 1 + count_n_vertices, i * this.stacks + j + 1 + count_n_vertices);
+                }
 
-        // }
+                this.normals.push(x / this.r, y / this.r, z / this.r);
+                this.texCoords1 = [
+                    0, 0,
+                    0, 1,
+                    1, 0,
+                    1, 1
+                ];
+                this.texCoords.push(...this.texCoords1);
+
+            }
+
+
+            if (i < this.stacks - 1) {
+                j--;
+                //this.indices.push(i * this.stacks + j+count_n_vertices,  (i + 1) * this.stacks + j+1+count_n_vertices,i * this.stacks+count_n_vertices);
+                //this.indices.push((i + 1) * this.stacks + j, i * this.stacks, (i + 1) * this.stacks);
+            }
+
+        }
+
+        this.vertices.push(0, 0, -this.r);
+        this.normals.push(0, 0, -1);
+        i--;
+        for (var k = 0; k < this.slices - 1; k++) {
+
+
+
+        }
 
 
 
