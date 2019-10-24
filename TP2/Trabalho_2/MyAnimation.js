2224fb0997538605319c8d51800addcd895274bc
unknown <up201704618@FEUPSIG.FE.UP.PT>
2019-10-24 12:33:03 +0100

    Animations class e frame in right place

---
+++ TP2/Trabalho_2/MyAnimation.js
@@ -0,0 +1,23 @@
+/**
+ * MyAnimation
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyAnimation extends CGFobject {
+	constructor(scene) {
+		super(scene);
+        this.initBuffers();
+       
+    }
+    update(t){
+        //altera a posiçao das asas
+        this.keyanimation
+    }
+    
+    apply(t){
+
+
+    }
+
+}
+
