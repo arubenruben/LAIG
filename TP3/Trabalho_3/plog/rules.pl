db53095e2e27c2d9da236d8a755ee271f9671780
unknown <almeidaruben35@gmail.com>
2019-12-30 11:59:58 +0000

    PROLOG Fixed?

--- TP3/Trabalho_3/plog/rules.pl
+++ TP3/Trabalho_3/plog/rules.pl
@@ -18,34 +18,25 @@ is_play_valid(Board_Out_temp,Coord_X,Coord_Y):-
                                                 Coord_Y_aux_cima is Coord_Y-1,
                                                 Coord_Y_aux_baixo is Coord_Y+1,
 
-
-                                                !, is_not_surronded(Board_Out_temp,Coord_X,Coord_Y),
-
-
                                                 %peca_esquerda
                                                 %se falhar uma peca, deve falhar o predicado inteiro, e esse o objetivo de usar cuts em todas as iteracoes
                                                 !,
                                                 is_a_piece_secure(Board_Out_temp,Coord_X_aux_esq,Coord_Y),
-
                                                 %dir%
                                                 !,
                                                 is_a_piece_secure(Board_Out_temp,Coord_X_aux_dir,Coord_Y),
-
                                                 %cima%
                                                 !,
                                                 is_a_piece_secure(Board_Out_temp,Coord_X,Coord_Y_aux_cima),
-
                                                 %baixo%
                                                 !,
                                                 is_a_piece_secure(Board_Out_temp,Coord_X,Coord_Y_aux_baixo),
-
                                                 %outro_cima%
                                                 !,     
                                                 if_then_else_aux(Even == 0,
                                                       is_a_piece_secure(Board_Out_temp,Coord_X_aux_esq,Coord_Y_aux_cima),
                                                       is_a_piece_secure(Board_Out_temp,Coord_X_aux_dir,Coord_Y_aux_cima)
                                                 ),
-
                                                 %outro_baixo%
                                                 !,
                                                 if_then_else_aux(Even == 0,
@@ -55,48 +46,48 @@ is_play_valid(Board_Out_temp,Coord_X,Coord_Y):-
                                                 
 
 is_not_surronded(Board, Coord_X, Coord_Y):-
-    Even is Coord_Y mod 2,
+                                                Even is Coord_Y mod 2,
 
-    Coord_X_aux_esq is Coord_X-1,
-    Coord_X_aux_dir is Coord_X+1,
-    Coord_Y_aux_cima is Coord_Y-1,
-    Coord_Y_aux_baixo is Coord_Y+1,
+                                                Coord_X_aux_esq is Coord_X-1,
+                                                Coord_X_aux_dir is Coord_X+1,
+                                                Coord_Y_aux_cima is Coord_Y-1,
+                                                Coord_Y_aux_baixo is Coord_Y+1,
 
-    Counter is 6,
+                                                Counter is 6,
 
-    %esq%
-    get_board(Board,Coord_X_aux_esq,Coord_Y,Element_1),
-    
-    %dir%
-    get_board(Board,Coord_X_aux_dir,Coord_Y,Element_2),
+                                                %esq%
+                                                get_board(Board,Coord_X_aux_esq,Coord_Y,Element_1),
+                                                
+                                                %dir%
+                                                get_board(Board,Coord_X_aux_dir,Coord_Y,Element_2),
 
-    %cima%
-    get_board(Board,Coord_X,Coord_Y_aux_cima,Element_3),
+                                                %cima%
+                                                get_board(Board,Coord_X,Coord_Y_aux_cima,Element_3),
 
-    %baixo%
-    get_board(Board,Coord_X,Coord_Y_aux_baixo,Element_4),
+                                                %baixo%
+                                                get_board(Board,Coord_X,Coord_Y_aux_baixo,Element_4),
 
-    %outro_cima%
-    if_then_else_aux(Even == 0,
-          get_board(Board,Coord_X_aux_esq,Coord_Y_aux_cima,Element_5),
-          get_board(Board,Coord_X_aux_dir,Coord_Y_aux_cima,Element_5)
-    ),
+                                                %outro_cima%
+                                                if_then_else_aux(Even == 0,
+                                                      get_board(Board,Coord_X_aux_esq,Coord_Y_aux_cima,Element_5),
+                                                      get_board(Board,Coord_X_aux_dir,Coord_Y_aux_cima,Element_5)
+                                                ),
 
-    %outro_baixo%
-    if_then_else_aux(Even == 0,
-          get_board(Board,Coord_X_aux_esq,Coord_Y_aux_baixo,Element_6),
-          get_board(Board,Coord_X_aux_dir,Coord_Y_aux_baixo,Element_6)
-    ),
+                                                %outro_baixo%
+                                                if_then_else_aux(Even == 0,
+                                                      get_board(Board,Coord_X_aux_esq,Coord_Y_aux_baixo,Element_6),
+                                                      get_board(Board,Coord_X_aux_dir,Coord_Y_aux_baixo,Element_6)
+                                                ),
 
-    is_an_element(Element_1,Counter,Counter_1),
-    is_an_element(Element_2,Counter_1,Counter_2),
-    is_an_element(Element_3,Counter_2,Counter_3),
-    is_an_element(Element_4,Counter_3,Counter_4),
-    is_an_element(Element_5,Counter_4,Counter_5),
-    is_an_element(Element_6,Counter_5,Counter_6),
-    
-    !,
-    Counter_6 \= 0.
+                                                is_an_element(Element_1,Counter,Counter_1),
+                                                is_an_element(Element_2,Counter_1,Counter_2),
+                                                is_an_element(Element_3,Counter_2,Counter_3),
+                                                is_an_element(Element_4,Counter_3,Counter_4),
+                                                is_an_element(Element_5,Counter_4,Counter_5),
+                                                is_an_element(Element_6,Counter_5,Counter_6),
+                                                
+                                                !,
+                                                Counter_6 \= 0.
 
 
 /*E um elemento valido decremento o counter*/
